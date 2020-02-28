2d82b3c66ec2c2999161455fe444266d099f26f7
Shaan <Shaan@admins-MacBook-Pro.local>
2020-02-27 16:52:44 -0800

    fixing links and padding

--- waypoint.js
+++ waypoint.js
@@ -1,3 +1,4 @@
+AOS.init();
 ['header', 'skills', 'work-experience', 'lnks', 'contact'].forEach(function(id) {
     new Waypoint({
       element: document.getElementById(id),
@@ -25,30 +26,44 @@
   // -------------------------------------------
   // Scroll to Smooth
 
-// Get a reference to the body
-const body = document.querySelector('body')
-// Listen for clicks
-body.addEventListener('click', function(e) {
-console.log(e.target)
+  // Get a reference to the body
+  const body = document.querySelector('body')
+  // Listen for clicks
+  body.addEventListener('click', function(e) {
+    console.log(e.target)
 
-// If a link has external class ignore it
-if (e.target.matches('.external')) {
-return
-}
+    // If a link has external class ignore it
+    if (e.target.closest('a').matches('.external')) {
+      return
+    }
 
-// Prevent the default behavior
-e.preventDefault()
+    // Prevent the default behavior
+    e.preventDefault()
 
-// Prepare to scroll
-// Get the href from the target
-const href = e.target.closest('a').href
-if (href) {
-// Select the current link
-selectNav(href)
-// Scroll to the id that matches the href
-const id = href.split('#')[1]
-document.getElementById(id).scrollIntoView({ 
-  behavior: 'smooth' 
-});
-} 
+    // Prepare to scroll
+    // Get the href from the target
+    const a = e.target.closest('a')
+    if (a === null) {
+      return
+    }
+
+    const href = a.closest('a').href
+    if (href) {
+      // Select the current link
+      selectNav(href)
+      // Scroll to the id that matches the href
+      const id = href.split('#')[1]
+      document.getElementById(id).scrollIntoView({ 
+        behavior: 'smooth' 
+      });
+    } 
 })
+
+
+// Extrnal links: go outside this page
+{/* <a href="google.com" class="external">Google</a> */}
+// internal navigate within the page
+{/* <a href="#header">Top</a> */}
+
+
+
